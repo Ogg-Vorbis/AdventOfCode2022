f336ba7b3612e9713f23f4bddf993442e3f4575b
Ogg-Vorbis <github@oggvorbis.net>
2022-12-01 09:53:12 -0800

    Init and Day one

---
+++ DayOne/Answer.cs
@@ -0,0 +1,20 @@
+﻿namespace Solutions.DayOne
+{
+    public class Answer
+    {
+        public static List<int> GetCaloriesPerElf(string[] input)
+        {
+            List<int> result = new();
+            string combinedInput = string.Join(",", input);
+            string[] separatedStrings = combinedInput.Split(",,");
+
+            foreach (var seq in separatedStrings)
+            {
+                var splitArr = seq.Split(",");
+                var sum = splitArr.Select(s => int.Parse(s)).Sum();
+                result.Add(sum);
+            }
+            return result;
+        }
+    }
+}
